1dfed420fdd760e23519ddb6b9eb0e5fa02c8cf4
HWS-Ted <f772886@gmail.com>
2024-07-16 17:03:56 +0800

    part4 page6

--- Ted/post.js
+++ Ted/post.js
@@ -1,5 +1,6 @@
 // 取得 input 資料
 const form = document.querySelector('#post-form')
+const formContainer = document.querySelector('.form-container')
 const postList = document.querySelector('.post-list')
 const postName = document.querySelector('.post-name')
 const postLocation = document.querySelector('.post-location')
@@ -18,6 +19,7 @@ const postPhone = document.querySelector('.post-phone')
 const postMail = document.querySelector('.post-mail')
 const publishBtn = document.querySelector('.publish-btn')
 const editBtn = document.querySelector('.edit-btn')
+const modalOverlay = document.querySelector('.modal-overlay')
 
 import { app } from './firebaseConfig.js'
 
@@ -25,7 +27,11 @@ import { app } from './firebaseConfig.js'
 import {
   getDatabase,
   ref,
-  set
+  set,
+  onValue,
+  remove,
+  get,
+  update
 } from 'https://www.gstatic.com/firebasejs/10.12.1/firebase-database.js'
 
 // 預期儲存的節點
@@ -71,6 +77,46 @@ function writePost(
   })
 }
 
+// 更新 post
+function updatePost(
+  postId,
+  name,
+  location,
+  background,
+  squareUrl,
+  topic,
+  preface,
+  content,
+  technology,
+  totalDay,
+  weekHour,
+  classType,
+  teachWay,
+  fee,
+  phone,
+  mail
+) {
+  const db = getDatabase(app)
+  const postRef = ref(db, 'posts/' + postId)
+  update(postRef, {
+    name,
+    city: location,
+    rectangleUrl: background,
+    squareUrl,
+    topic,
+    preface,
+    content,
+    technology,
+    totalDay,
+    weekHour,
+    classType,
+    teachWay,
+    fee,
+    phone,
+    mail
+  })
+}
+
 // 生成隨機 id
 function generateRandomId() {
   let id =
@@ -82,32 +128,115 @@ function generateRandomId() {
 
 form.addEventListener('submit', event => {
   event.preventDefault()
-  writePost(
-    postName.value,
-    postLocation.value,
-    postBackground.value,
-    postSquareUrl.value,
-    postTopic.value,
-    postPreface.value,
-    postContent.value,
-    postTechnology.value,
-    postTotalDay.value,
-    postWeekHour.value,
-    postClassType.value,
-    postTeachWay.value,
-    postFee.value,
-    postPhone.value,
-    postMail.value
-  )
+  const postId = form.dataset.id || null
+  const postData = {
+    name: postName.value,
+    location: postLocation.value,
+    background: postBackground.value,
+    squareUrl: postSquareUrl.value,
+    topic: postTopic.value,
+    preface: postPreface.value,
+    content: postContent.value,
+    technology: postTechnology.value,
+    totalDay: postTotalDay.value,
+    weekHour: postWeekHour.value,
+    classType: postClassType.value,
+    teachWay: postTeachWay.value,
+    fee: postFee.value,
+    phone: postPhone.value,
+    mail: postMail.value
+  }
+  if (postId) {
+    updatePost(postId, ...Object.values(postData))
+  } else {
+    writePost(...Object.values(postData))
+  }
+
   form.reset()
+  formContainer.style.display = 'none'
+  modalOverlay.style.display = 'none'
 })
 
+function renderPostList() {
+  const db = getDatabase(app)
+  const postRef = ref(db, 'posts/')
+  onValue(postRef, snapshot => {
+    postList.innerHTML = ''
+    const classArray = Object.values(snapshot.val())
+    let template = ``
+    classArray.forEach(item => {
+      template = `<div class="post-list-class">
+              <h3 class="className">${item.name}</h3>
+              <div class="post-btns">
+                <button class="btn-primary post-btn-edit" data-id="${item.uid}">編輯</button>
+                <button class="btn-primary post-btn-del" data-id="${item.uid}">刪除</button>
+              </div>
+            </div>`
+      postList.innerHTML += template
+    })
+  })
+}
+renderPostList()
+
+function deletePost(postId) {
+  const db = getDatabase(app)
+  const postRef = ref(db, `posts/${postId}`)
+  remove(postRef)
+    .then(() => {
+      alert('貼文已成功刪除')
+      renderPostList()
+    })
+    .catch(error => {
+      console.error('刪除貼文失敗:', error)
+    })
+}
+
 publishBtn.addEventListener('click', () => {
-  postList.style.display = 'none'
-  form.style.display = 'grid'
+  formContainer.style.display = 'block'
+  modalOverlay.style.display = 'block'
 })
 
 editBtn.addEventListener('click', () => {
-  form.style.display = 'none'
+  formContainer.style.display = 'none'
   postList.style.display = 'block'
+})
+
+modalOverlay.addEventListener('click', () => {
+  formContainer.style.display = 'none'
+  // 表格內文字清空
+  form.reset()
+  delete form.dataset.id
+})
+
+postList.addEventListener('click', e => {
+  const postId = e.target.getAttribute('data-id')
+  if (e.target.classList.contains('post-btn-del')) {
+    deletePost(postId)
+  } else if (e.target.classList.contains('post-btn-edit')) {
+    const db = getDatabase(app)
+    const postRef = ref(db, `posts/${postId}`)
+
+    form.dataset.id = postId
+    formContainer.style.display = 'block'
+    modalOverlay.style.display = 'block'
+
+    get(postRef).then(snapshot => {
+      const post = snapshot.val()
+      postName.value = post.name
+      postLocation.value = post.city
+      postBackground.value = post.rectangleUrl
+      postSquareUrl.value = post.squareUrl
+      postTopic.value = post.topic
+      postPreface.value = post.preface
+      postContent.value = post.content
+      postTechnology.value = post.technology
+      postTotalDay.value = post.totalDay
+      postWeekHour.value = post.weekHour
+      postClassType.value = post.classType
+      postTeachWay.value = post.teachWay
+      postFee.value = post.fee
+      postPhone.value = post.phone
+      postMail.value = post.mail
+    })
+  }
 })
